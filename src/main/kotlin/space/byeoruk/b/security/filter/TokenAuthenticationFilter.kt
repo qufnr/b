a6a7a004863afac81035c5ef2c4331a37376cd75
qufnr <qufnr@kakao.com>
2025-12-03 14:40:35 +0900

    modified: 요청 필터에서 Bearer 토큰이 있을 때만 계정 검증하도록 수정

--- src/main/kotlin/space/byeoruk/b/security/filter/TokenAuthenticationFilter.kt
+++ src/main/kotlin/space/byeoruk/b/security/filter/TokenAuthenticationFilter.kt
@@ -17,21 +17,25 @@ class TokenAuthenticationFilter(private val tokenProvider: TokenProvider, privat
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         try {
-            val token = tokenProvider.getBearerToken(request.getHeader("Authorization"))
+            val bearerToken = request.getHeader("Authorization")
 
-            if(tokenProvider.isValidToken(token) && tokenProvider.getTokenType(token) == TokenType.ACCESS) {
-                val payload = tokenProvider.getTokenPayload(token)
-                val memberDetails = memberDetailsService.loadUserByUsername(payload["id"] as String)
+            //  Header 의 Authentication 에 Bearer 토큰이 있을 때만 검증 수행
+            if(bearerToken != null && bearerToken.isNotBlank()) {
+                val token = tokenProvider.getBearerToken(bearerToken)
+                if(tokenProvider.isValidToken(token) && tokenProvider.getTokenType(token) == TokenType.ACCESS) {
+                    val payload = tokenProvider.getTokenPayload(token)
+                    val memberDetails = memberDetailsService.loadUserByUsername(payload["id"] as String)
 
-                if(!memberDetails.isAccountNonLocked)
-                    throw BadCredentialsException("차단된 계정입니다. 관리자에게 문의해 주세요.")
+                    if(!memberDetails.isAccountNonLocked)
+                        throw BadCredentialsException("차단된 계정입니다. 관리자에게 문의해 주세요.")
 
-                if(!memberDetails.isEnabled)
-                    throw BadCredentialsException("비활성화 상태인 계정입니다. 활성화 하려면 계정을 다시 인증해 주세요.")
+                    if(!memberDetails.isEnabled)
+                        throw BadCredentialsException("비활성화 상태인 계정입니다. 활성화 하려면 계정을 다시 인증해 주세요.")
 
-                val authentication = UsernamePasswordAuthenticationToken(memberDetails, "", memberDetails.authorities)
+                    val authentication = UsernamePasswordAuthenticationToken(memberDetails, "", memberDetails.authorities)
 
-                SecurityContextHolder.getContext().authentication = authentication
+                    SecurityContextHolder.getContext().authentication = authentication
+                }
             }
         }
         catch(e: Exception) {
