608d21f68b6cf07d081b17dbacfcb50bf6cb3795
qufnr <qufnr@kakao.com>
2025-12-03 09:42:16 +0900

    added: MemberAvatarProvider.kt, MemberBannerProvider.kt 주석 추가

--- src/main/kotlin/space/byeoruk/b/domain/member/provider/MemberAvatarProvider.kt
+++ src/main/kotlin/space/byeoruk/b/domain/member/provider/MemberAvatarProvider.kt
@@ -7,7 +7,6 @@ import space.byeoruk.b.domain.member.entity.Member
 import space.byeoruk.b.domain.member.exception.MemberAvatarNotFoundException
 import space.byeoruk.b.domain.member.exception.MemberResourceTransferFailedException
 import space.byeoruk.b.global.provider.EntityFileProvider
-import space.byeoruk.b.global.utility.FileUtilities
 import space.byeoruk.b.global.provider.ServerResourceProvider
 
 @Component
@@ -22,6 +21,15 @@ class MemberAvatarProvider(
     //  서버 리소스 디렉터리 내 상위 디렉터리 명
     private final val directory = "avatar/"
 
+    /**
+     * 계정 아바타 서버에 저장
+     *
+     * @param entity 사용자 Entity
+     * @param file 멀티파트 파일
+     * @return 저장된 파일 명
+     * @throws space.byeoruk.b.global.exception.FileUtilityException 파일 검증 실패 시 던짐
+     * @throws MemberResourceTransferFailedException 파일 저장 실패 시 던짐
+     */
     override fun save(entity: Member, file: MultipartFile): String {
         if(file.isEmpty)
             throw MemberAvatarNotFoundException()
@@ -41,6 +49,12 @@ class MemberAvatarProvider(
         }
     }
 
+    /**
+     * 계정 아바타 서버에서 삭제
+     *
+     * @param entity 사용자 Entity
+     * @return 삭제되었다면 `true` 아니면 `false` 반환
+     */
     override fun delete(entity: Member): Boolean {
         return (entity.avatar != null && entity.avatar!!.isNotBlank()) &&
                 serverResourceProvider.delete("${serverResourceProvider.path}${directory}${entity.avatar}")
--- src/main/kotlin/space/byeoruk/b/domain/member/provider/MemberBannerProvider.kt
+++ src/main/kotlin/space/byeoruk/b/domain/member/provider/MemberBannerProvider.kt
@@ -20,6 +20,15 @@ class MemberBannerProvider(
 ): EntityFileProvider<Member> {
     private final val directory = "banner/"
 
+    /**
+     * 계정 배너 파일 서버에 저장
+     *
+     * @param entity 사용자 Entity
+     * @param file 멀티파트 파일
+     * @return 저장된 파일 명
+     * @throws space.byeoruk.b.global.exception.FileUtilityException 파일 검증 실패 시 던짐
+     * @throws MemberResourceTransferFailedException 파일 저장 실패 시 던짐
+     */
     override fun save(entity: Member, file: MultipartFile): String {
         if(file.isEmpty)
             throw MemberBannerNotFoundException()
@@ -38,6 +47,12 @@ class MemberBannerProvider(
         }
     }
 
+    /**
+     * 계정 배너 파일 서버에서 삭제
+     *
+     * @param entity 계정 Entity
+     * @return 삭제되었다면 `true` 아니면 `false` 반환
+     */
     override fun delete(entity: Member): Boolean {
         return (entity.avatar != null && entity.avatar!!.isNotBlank()) &&
                 serverResourceProvider.delete("${serverResourceProvider.path}${directory}${entity.avatar}")
