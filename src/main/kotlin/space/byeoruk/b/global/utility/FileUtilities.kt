a0c875353f88cd709a1c856b18838cbb8023b270
qufnr <qufnr@kakao.com>
2026-02-10 17:54:37 +0900

    modified: 파일 크기 검증 로직 수정

--- src/main/kotlin/space/byeoruk/b/global/utility/FileUtilities.kt
+++ src/main/kotlin/space/byeoruk/b/global/utility/FileUtilities.kt
@@ -1,7 +1,10 @@
 package space.byeoruk.b.global.utility
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.multipart.MultipartFile
 import space.byeoruk.b.global.exception.FileUtilityException
+
+private val log = KotlinLogging.logger {}
 
 object FileUtilities {
     /**
@@ -14,21 +17,21 @@ object FileUtilities {
     fun validateExtension(file: MultipartFile, extensions: List<String>) {
         val extension = getExtension(file)
         if(!extensions.contains(extension))
-            throw FileUtilityException("파일 확장자가 올바르지 않습니다.")
+            throw FileUtilityException("파일 확장자가 올바르지 않습니다. ($extension)")
     }
 
     /**
      * 파일 크기 검증
      *
      * @param file 멀티파트 파일
-     * @param size 파일 크기
+     * @param maxFileSize 파일 크기 (MB)
      * @throws FileUtilityException 파일 크기가 초과되면 던짐
      */
-    fun validateSize(file: MultipartFile, size: Long) {
-        if(file.size > size) {
-            val fileSize = file.size / 1000 / 1000
-            val maximumFileSize = size / 1000 / 1000
-            throw FileUtilityException("파일 크기를 초과했습니다. ($fileSize / $maximumFileSize MB)")
+    fun validateSize(file: MultipartFile, maxFileSize: Long) {
+        val maxFileSizeBytes = maxFileSize * 1024 * 1024
+
+        if(file.size > maxFileSizeBytes) {
+            throw FileUtilityException("파일 크기를 초과했습니다. (${file.size} / $maxFileSizeBytes Bytes)")
         }
     }
 
